317c9e22c5d77251ba47603f3a4624ec0d673638
Michael Chung <michael.chung1994@gmail.com>
2018-12-04 03:35:23 +0000

    passed the request error .on function

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,16 @@
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  request.get('https://api.github.com/repos/jquery/jquery/contributors')
+
+         .on('error', function (err) {                                   // Note 2
+            throw err;
+         })
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
